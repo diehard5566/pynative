abc43d5381a849fcc8ba5f6270e97efdc16195ba
diehard5566 <shihyao001@gmail.com>
2022-01-28 21:18:38 +0800

    multi number

---
+++ Loop-exercise/Exericise4.py
@@ -0,0 +1,10 @@
+# Exercise 4: Write a program to print multiplication table of a given number
+
+
+multi = int(input("num = "))
+count = multi
+while count <= 20:
+    print(count)
+    count = count + multi 
+
+
