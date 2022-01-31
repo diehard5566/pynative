067742900905fdecb349d90765185d9b06451998
diehard5566 <shihyao001@gmail.com>
2022-01-31 17:22:49 +0800

    patterns+patterns

---
+++ Loop-exercise/Exercise17.py
@@ -0,0 +1,16 @@
+# Exercise 17: Find the sum of the series upto n terms
+# Write a program to calculate the sum of series up to n term. 
+# For example, if n =5 the series will become 2 + 22 + 222 + 2222 + 22222 = 24690
+
+# Given:
+
+# # number of terms
+# n = 5
+
+num = int(input("Given number: "))
+
+sum = 2
+count = 0
+while count <= (sum + (sum * 10))*(num-1):
+    print(count)
+    count = count + ((sum + (sum * 10))*(num-1))
