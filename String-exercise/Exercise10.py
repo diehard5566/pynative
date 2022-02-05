7b0dd35bc62bd502e6a6a300f06814bedd76dcda
diehard5566 <shihyao001@gmail.com>
2022-02-06 01:02:41 +0800

    想不出來10

---
+++ String-exercise/Exercise10.py
@@ -0,0 +1,14 @@
+# Exercise 10: Write a program to count occurrences of all characters within a string
+# Given:
+
+str1 = "Apple"
+
+count = {}
+
+c = str1.count("A")
+
+print(c)
+
+
+
+print(count)
