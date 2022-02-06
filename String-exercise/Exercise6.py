8567d0a0050a781889b26b579192166d1a33773e
diehard5566 <shihyao001@gmail.com>
2022-02-06 23:41:08 +0800

    e6 正解

--- String-exercise/Exercise6.py
+++ String-exercise/Exercise6.py
@@ -5,14 +5,35 @@
 
 # Given:
 
-# s1 = "Abc"
-# s2 = "Xyz"
+s1 = "Abcgr"
+s2 = "Xyzq"
 
-def mixed(s1, s2):
-    mix1 = s1[0] + s2[-1]
-    mix2 = s1[1] + s2[-2]
-    mix3 = s1[2] + s2[-3]
-    mix = mix1 + mix2 + mix3
-    print(mix)
+# def mixed(s1, s2):
+#     mix1 = s1[0] + s2[-1]
+#     mix2 = s1[1] + s2[-2]
+#     mix3 = s1[2] + s2[-3]
+#     mix = mix1 + mix2 + mix3
+#     print(mix)
 
-mixed("Abc", "Xyz")
+# mixed("Abc", "Xyz")
+
+# get string length
+s1_length = len(s1)
+s2_length = len(s2)
+
+# get length of a bigger string
+length = s1_length if s1_length > s2_length else s2_length
+result = ""
+print(length)
+# reverse s2
+s2 = s2[::-1]
+
+# iterate string 
+# s1 ascending and s2 descending
+for i in range(length):
+    if i < s1_length:
+        result = result + s1[i]
+    if i < s2_length:
+        result = result + s2[i]
+    
+print(result)
