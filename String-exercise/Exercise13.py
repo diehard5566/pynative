2fb85e71460c0bb57fd6a19b749342f374401588
diehard5566 <shihyao001@gmail.com>
2022-02-06 13:58:50 +0800

    exercise13

---
+++ String-exercise/Exercise13.py
@@ -0,0 +1,14 @@
+# Exercise 13: Split a string on hyphens
+# Write a program to split a given string on hyphens and display each substring.
+
+# Given:
+
+str1 = "Emma-is-a-data-scientist"
+
+str2 = str1.split("-")
+
+print("Displaying each substring", "\n")
+for i in str2:
+    print(i)
+
+
