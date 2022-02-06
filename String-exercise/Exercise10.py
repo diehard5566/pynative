415411c964f23332ad1eedd734d7a06c160054e8
diehard5566 <shihyao001@gmail.com>
2022-02-07 00:23:05 +0800

    e10/e14解

--- String-exercise/Exercise10.py
+++ String-exercise/Exercise10.py
@@ -1,14 +1,13 @@
 # Exercise 10: Write a program to count occurrences of all characters within a string
 # Given:
 
-str1 = "Apple"
+str1 = "apple"
 
-count = {}
+# create a result dictionary
+char_dict = dict()
 
-c = str1.count("A")
-
-print(c)
-
-
-
-print(count)
+for char in str1:
+    count = str1.count(char)
+    # add / update the count of a character
+    char_dict[char] = count # char = key , count = value 
+    print('Result:', char_dict)
--- String-exercise/Exercise14.py
+++ String-exercise/Exercise14.py
@@ -4,8 +4,16 @@
 str_list = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
 
 
-str_list.remove("")
-str_list.remove("")
-str_list.remove(None)
+# str_list.remove("")
+# str_list.remove("")
+# str_list.remove(None)
 
+# print(str_list)
+
+for i in str_list:
+    if i == "":
+        str_list.remove(i)
+    elif i == None:
+        str_list.remove(i)
+        
 print(str_list)
