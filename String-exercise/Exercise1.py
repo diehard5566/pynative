14aa93337e8b53c23e09e4c3bf28b913e1e2ebfa
diehard5566 <shihyao001@gmail.com>
2022-02-06 21:00:31 +0800

    exercise1另解

--- String-exercise/Exercise1.py
+++ String-exercise/Exercise1.py
@@ -5,4 +5,24 @@
 
 str1 = "James"
 
-print(str1[0] + str1[2] + str1[4])
+mi = int((len(str1)/2))
+
+print(str1[0] + str1[mi] + str1[-1])
+
+str1 = 'James'
+print("Original String is", str1)
+
+# Get first character
+res = str1[0]
+
+# Get string size
+l = len(str1)
+# Get middle index number
+mi = int(l / 2)
+# Get middle character and add it to result
+res = res + str1[mi]
+
+# Get last character and add it to result
+res = res + str1[l - 1]
+
+print("New String:", res)
