d7f7c15244c44c3f4fa2ed5073786bde2a16b072
diehard5566 <shihyao001@gmail.com>
2022-02-07 15:22:05 +0800

    exercise5

---
+++ Function-exercise/Exercise5.py
@@ -0,0 +1,14 @@
+# Exercise 5: Create an inner function to calculate the addition in the following way
+# Create an outer function that will accept two parameters, a and b
+# Create an inner function inside an outer function that will calculate the addition of a and b
+# At last, an outer function will add 5 into addition and return it
+
+def outer(a, b):
+    def inner(a, b):
+        sum = a + b
+        return sum
+    fin = inner(a, b) + 5
+    print(fin)
+
+outer(5,2) 
+    
