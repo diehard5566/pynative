75906784657e5e344aa88ba298f9684bd19da65c
diehard5566 <shihyao001@gmail.com>
2022-02-07 14:45:13 +0800

    Function-exercise

---
+++ Function-exercise/Exercise1.py
@@ -0,0 +1,7 @@
+# Exercise 1: Create a function in Python
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+
+def name_age(name, age):
+    print(name, age)
+
+name_age("Max", 31)
