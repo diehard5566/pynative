c6bf9597ec78e80f867112f29f915de66fa17827
diehard5566 <shihyao001@gmail.com>
2022-02-07 21:35:12 +0800

    set exercise6

--- Data structure-exercise/Exercise6.py
+++ Data structure-exercise/Exercise6.py
@@ -6,7 +6,8 @@
 first_set = {23, 42, 65, 57, 78, 83, 29}
 second_set = {57, 83, 29, 67, 73, 43, 48}
 
+#用Intersection方法 找出兩個set有交集的地方
 new = first_set & second_set
 print(new)
-
+#用difference方法 A-B可以得出AB相同地方以外的數值
 print(first_set - second_set)
