8d06d2c0820772b1a80563a05a2ba812ddc35da6
diehard5566 <shihyao001@gmail.com>
2022-02-07 22:06:20 +0800

    增加備註

--- Data structure-exercise/Exercise7.py
+++ Data structure-exercise/Exercise7.py
@@ -3,7 +3,7 @@
 
 first_set = {27, 43, 34}
 second_set = {34, 93, 22, 27, 43, 53, 48}
-
+#用issubset/issuperset方法來檢查true/false
 print(first_set.issubset(second_set))
 
 print(second_set.issubset(first_set))
@@ -11,7 +11,7 @@ print(second_set.issubset(first_set))
 print(first_set.issuperset(second_set))
 
 print(second_set.issuperset(first_set))
-
+#檢查是否為subset,是就clear掉
 if first_set.issubset(second_set):
     first_set.clear()
 
--- Data structure-exercise/Exercise8.py
+++ Data structure-exercise/Exercise8.py
@@ -7,7 +7,9 @@ roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
 sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
 
 new = []
+#先把dict iterate
 for j in sample_dict.items():
+    #再把list iterate
     for i in roll_number:
         if i in j:
             new.append(i)
