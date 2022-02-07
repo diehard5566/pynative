affbb4e0c51a59c4ee817e9504bdfe8d24f2c71c
diehard5566 <shihyao001@gmail.com>
2022-02-07 15:58:04 +0800

    exercise5-遞迴

---
+++ Function-exercise/Exercise6.py
@@ -0,0 +1,26 @@
+# Exercise 6: Create a recursive function
+# Write a program to create a recursive function to calculate the sum of numbers from 0 to 10.
+
+# A recursive function is a function that calls itself, again and again.
+
+# Expected Output:
+
+# 55
+
+# def recu(num):
+#     count = 0
+#     for i in range(0, num + 1):
+#         count = count + i
+#     return count
+
+# print(recu(10))
+
+def addition(num):
+    if num:
+        # call same function by reducing number by 1
+        return num + addition(num - 1)
+    else:
+        return 0
+
+res = addition(10)
+print(res)
