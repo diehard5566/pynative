e83672e6229bc1e2a027c7b3ff4b49285765184d
diehard5566 <shihyao001@gmail.com>
2022-02-07 21:55:47 +0800

    exercise8 dict

---
+++ Data structure-exercise/Exercise8.py
@@ -0,0 +1,15 @@
+# Exercise 8: Iterate a given list and check if a given element exists as a key’s value in a dictionary. 
+# If not, delete it from the list
+# Given:
+
+
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+
+new = []
+for j in sample_dict.items():
+    for i in roll_number:
+        if i in j:
+            new.append(i)
+print(new)
+
