14596e537d7a1649ee7a11add7b3e51df41c4453
diehard5566 <shihyao001@gmail.com>
2022-02-07 20:45:03 +0800

    exercise5改

--- Data structure-exercise/Exercise5.py
+++ Data structure-exercise/Exercise5.py
@@ -4,13 +4,17 @@
 first_list = [2, 3, 4, 5, 6, 7, 8]
 second_list = [4, 9, 16, 25, 36, 49, 64]
 
-res = []
+# res = []
 
-for i in first_list:
-    for j in second_list:
-        if j == i*i:
-            res.append((i,j))
-            set_res = set(res)
+# for i in first_list:
+#     for j in second_list:
+#         if j == i*i:
+#             res.append((i,j))
+#             set_res = set(res)
 
-print(set_res)
+# print(set_res)
+
+res = zip(first_list, second_list)
+ress = set(res)
+print(ress)
 
