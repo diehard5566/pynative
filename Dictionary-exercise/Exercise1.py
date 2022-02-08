3995e05400712686fe2d09c2f0c2bb146b138c22
diehard5566 <shihyao001@gmail.com>
2022-02-08 15:27:10 +0800

    exercise1

---
+++ Dictionary-exercise/Exercise1.py
@@ -0,0 +1,14 @@
+# Exercise 1: Convert two lists into a dictionary
+# Below are the two lists. Write a Python program to convert them into a dictionary 
+# in a way that item from list1 is the key and item from list2 is the value
+
+
+
+def dic(keys, values):
+    new = zip(keys , values)
+    print(new)
+    
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dic(keys, values)
