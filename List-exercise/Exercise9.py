a0cbcefa15bb77a5437568f2817c4d72bbee45dd
diehard5566 <shihyao001@gmail.com>
2022-02-08 15:06:27 +0800

    exercise9

---
+++ List-exercise/Exercise9.py
@@ -0,0 +1,13 @@
+# Exercise 9: Replace list’s item with new value if found
+# You have given a Python list. Write a program to find value 20 in the list, and if it is present, 
+# replace it with 200. Only update the first occurrence of an item.
+
+# Given:
+
+list1 = [5, 10, 15, 20, 25, 50, 20]
+
+ls = list1.index(20)
+
+list1[ls] = 200
+
+print(list1)
